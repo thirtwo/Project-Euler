b8f09ff8c7af842e87673d89b643c0a7011fb026
thirtwo <65511956+thirtwo@users.noreply.github.com>
2022-06-14 04:17:17 +0300

    add Multiples of 3 or 5

---
+++ Project-Euler/Multiplesof3or5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Project_Euler
+{
+    class Multiplesof3or5
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindSumOf3Or5Multiples(1000));
+        }
+
+        private static int FindSumOf3Or5Multiples(int max)
+        {
+            var sum = 0;
+            for (int i = 1; i < max; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                    sum += i; 
+            }
+            return sum;
+        }
+    }
+}
