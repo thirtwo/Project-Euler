c205ad7ccae8f544b433f659a45c9cd1f6146e00
thirtwo <65511956+thirtwo@users.noreply.github.com>
2022-06-15 04:52:21 +0300

    add Even Fibonacci Numbers

---
+++ Project-Euler/EvenFibonacciNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Project_Euler
+{
+    class EvenFibonacciNumbers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindSumOfEvenFibonacciNumbers(4000000));
+        }
+
+        static ulong FindSumOfEvenFibonacciNumbers(int max)
+        {
+            ulong sum = 0;
+            int lastFibonacci = 0, i = 1;
+            while (i < max)
+            {
+                var val = Convert.ToUInt64(i + lastFibonacci);
+                if (val % 2 == 0)
+                    sum += val;
+                var temp = i;
+                i += lastFibonacci;
+                lastFibonacci = temp;
+            }
+            return sum;
+        }
+    }
+}
